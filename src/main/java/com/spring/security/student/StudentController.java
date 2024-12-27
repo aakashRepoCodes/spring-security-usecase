bcd8d34015bc3e7ce9d3c2e475d2767606b65667
Aakash Chaturvedi <aakashch@mail.uni-paderborn.de>
2024-12-27 14:07:18 +0100

    integrated csrf token for POST request

---
+++ src/main/java/com/spring/security/student/StudentController.java
@@ -0,0 +1,46 @@
+package com.spring.security.student;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class StudentController {
+
+    List<Student> studentList = new ArrayList<>(Arrays.asList(
+            new Student(1, "Joy", "user@gmail.com"),
+            new Student(2, "Max", "user@gmail.com")
+    ));
+
+    @GetMapping("/get-token")
+    public CsrfToken getCsrfToken(HttpServletRequest request) {
+        CsrfToken csrfToken = (CsrfToken) request.getAttribute("_csrf");
+        if (csrfToken == null) {
+            throw new IllegalStateException("CSRF token is not available");
+        }
+        return csrfToken;
+
+    }
+
+    // By - default spring security blocks put/patch/post req without a valid csrf token
+    @PostMapping("/add-student")
+    public Student addStudent(@RequestBody Student student) {
+        studentList.add(student);
+        return student;
+    }
+
+    // By - default spring security keeps get request without requiring any auth/token
+    @GetMapping("/get-student")
+    public List<Student> getStudent() {
+        return studentList ;
+    }
+
+}
